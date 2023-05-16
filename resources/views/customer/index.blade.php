dff8dee01993ddd73659638f0719891289c9dc96
el mehdi <elmehdibennanikhir@gmail.com>
2023-05-16 15:28:54 +0100

    create user & admin navbar

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -2,32 +2,32 @@
 @section('content')
  
 <div class="row">
-{{-- @foreach ($products as $product)
+@foreach ($customers as $customer)
     
 
 <div class="col-4">
         <div class="container">
        
         <div class="card">
-            <img class="card-img-top  " src="{{asset('storage/'.$product->image)}}" alt="Title">
+            <img class="card-img-top  " src="{{asset('storage/'.$customer->image)}}" alt="Title">
             <div class="card-body">
-                <h4 class="card-title">{{$product->product_name}}</h4>
-                <h6 class="card-text text-success">{{$product->prix}}DH</h6>
+                <h4 class="card-title">{{$customer->product_name}}</h4>
+                <h6 class="card-text text-success">{{$customer->prix}}DH</h6>
                 <ul class="p-3">
-                    @if ($product->stock=='Available')    
-                    <li class="text-success">{{$product->stock}}</li>
+                    @if ($customer->stock=='Available')    
+                    <li class="text-success">{{$customer->stock}}</li>
                     @else
-                    <li class="text-danger">{{$product->stock}}</li>
+                    <li class="text-danger">{{$customer->stock}}</li>
                     @endif
                 </ul>
                 <div class="card-footer text-muted">
                     
-                    <p>{{$product->bio}}</p>
+                    <p>{{$customer->bio}}</p>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endforeach --}}
+@endforeach
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,10 @@
                                 </li>
                             @endif
                         @else
+                        
+                            
+                        @if (auth()->user()->is_admin===1)
+                            
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 Select
@@ -128,6 +132,27 @@
                                     </form>
                                 </div>
                             </li>
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endif
                         @endguest
                     </ul>
                 </div>
