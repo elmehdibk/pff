5333ea901438e87099d4d45e01bb2dbe23722dac
el mehdi <elmehdibennanikhir@gmail.com>
2023-06-25 14:52:44 +0100

    updated somthing in middleware -v3

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -15,7 +15,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(auth()->user()->is_admin===1){
+        if(auth()->check() && auth()->user()->is_admin == 1){
             return $next($request);
         }
         // return redirect('home')->with("error","you dant have access to admin");
--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::resource('products',ProductController::class);
-Route::get('/products/{type?}',[ProductController::class,'index'])->name('products.index');
-Route::get('/create',[ProductController::class,'create'])->name('products.create');
-Route::post('/products/store',[ProductController::class,'store'])->name('products.store');
-Route::get('/products/show/{product}',[ProductController::class,'show'])->name('products.show');
-Route::get('/products/{product}/edit',[ProductController::class,'edit'])->name('products.edit');
-Route::put('/products/{product}',[ProductController::class,'update'])->name('products.update');
-Route::get('/products/verification/{product}',[ProductController::class,'verification'])->name('products.verification');
-Route::get('/products/delete/{id}',[ProductController::class,'destroy'])->name('products.destroy');
-// Route::delete('/products/{product}',[ProductController::class,'destroy'])->name('products.destroy');
+
+Route::get('/products/{type?}',[ProductController::class,'index'])->name('products.index')->middleware('is_admin');
+Route::get('/create',[ProductController::class,'create'])->name('products.create')->middleware('is_admin');
+Route::post('/products/store',[ProductController::class,'store'])->name('products.store')->middleware('is_admin');
+Route::get('/products/show/{product}',[ProductController::class,'show'])->name('products.show')->middleware('is_admin');
+Route::get('/products/{product}/edit',[ProductController::class,'edit'])->name('products.edit')->middleware('is_admin');
+Route::put('/products/{product}',[ProductController::class,'update'])->name('products.update')->middleware('is_admin');
+Route::get('/products/verification/{product}',[ProductController::class,'verification'])->name('products.verification')->middleware('is_admin');
+Route::get('/products/delete/{id}',[ProductController::class,'destroy'])->name('products.destroy')->middleware('is_admin');
+
 
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
-Route::resource('customers',CustomerController::class);
+Route::resource('customers',CustomerController::class)->middleware('auth');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
